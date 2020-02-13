46d026b3dee91b22fc17b6e081c972d11fd73bcf
Hafiz Suara <hafbau@yahoo.com>
2020-02-13 06:55:59 -0700

    ready to deploy

--- app/server.py
+++ app/server.py
@@ -9,10 +9,12 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+# export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+# export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?id=1SrDduTBv8fq7O7P5Nsw0ZczwWR3XJlm5&authuser=0&export=download'
+export_file_name = 'resnet50.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['coyote', 'dog']
 path = Path(__file__).parent
 
 app = Starlette()
